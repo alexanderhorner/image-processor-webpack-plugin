69096565668f90c2db119cdf24fb73b57c96bb37
Alexander Horner <mail@alexanderhorner.com>
2021-03-30 21:00:42 +0200

    old emit asset

--- imageprocessor.js
+++ imageprocessor.js
@@ -7,23 +7,51 @@ var changedFile = [];
 
 class ImageProcessor {
     apply(compiler) {
-        compiler.hooks.emit.tapPromise('ImageProcessor', (compilation) => {
-            // return a Promise that resolves when we are done...
-            return new Promise((resolve, reject) => {
-                setTimeout(function () {
+        // compiler.hooks.emit.tapPromise('ImageProcessor', (compilation) => {
+        //     // return a Promise that resolves when we are done...
+        //     return new Promise((resolve, reject) => {
+        //         setTimeout(function () {
 
-                    const dependency = path.join(compiler.context, 'src/img')
-                    compilation.contextDependencies.add(dependency);
-                    compilation.fileDependencies.add(dependency + '/1test.txt');
+        //             const dependency = path.join(compiler.context, 'src/img')
+        //             compilation.contextDependencies.add(dependency);
+        //             compilation.fileDependencies.add(dependency + '/1test.txt');
 
                     
 
-                    ImageProcessor.compileFile(compilation)
+        //             ImageProcessor.compileFile(compilation)
 
-                    console.log('\x1b[41m', '\x1b[30m', 'Done with async work...', '\x1b[0m')
-                    resolve()
-                }, 50);
-            });
+        //             console.log('\x1b[41m', '\x1b[30m', 'Done with async work...', '\x1b[0m')
+        //             resolve()
+        //         }, 50);
+        //     });
+        // });
+
+        compiler.hooks.emit.tapAsync('FileListPlugin', (compilation, callback) => {
+            
+            const source = "test"
+
+            // Insert this list into the webpack build as a new file asset:
+            compilation.assets['test.png'] = {
+              source: function () {
+                return source
+              },
+              size: function () {
+                return source.length
+              },
+            };
+
+            // compilation.hooks.processAssets.tapPromise(
+            //     {
+            //       name: 'plugin-name',
+            //       // https://github.com/webpack/webpack/blob/master/lib/Compilation.js#L3280
+            //       stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
+            //     },
+            //     () => { 
+            //       compilation.emitAsset('test.js', new sources.RawSource(data))
+            //     }
+            // );
+      
+            callback();
         });
 
         compiler.hooks.watchRun.tap('WatchRun', (comp) => {
